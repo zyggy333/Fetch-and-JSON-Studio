7fb8b425dee21e4093bca0daae69d8de65a26296
Zyggy Buss <zyggy333@gmail.com>
2019-10-10 19:29:17 -0500

    Updated

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // TODO: add code here
+window.addEventListener("load", function() {
+		fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+			response.json().then( function(json) {
+				const div = document.getElementById("container");
+				for(let i=0;i<json.length;i++){
+					div.innerHTML+=`
+						<div class="astronaut">
+							<div class="bio">
+								<h3>${json[i].firstName} ${json[i].lastName}</h3>
+								<ul>
+									<li>Hours in space: ${json[i].hoursInSpace}</li>
+									<li>Active: ${json[i].active}</li>
+									<li>Skills: ${json[i].skills}</li>
+								</ul>
+							</div>
+							<img class="avatar" src="${json[i].picture}"></img>
+						</div> \n`
+				}
+		});
+});
+});
